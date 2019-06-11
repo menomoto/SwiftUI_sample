a9c13fa7f6dd38424e0af65e4e945dd80e1f1d60
menomoto <menomoto@menomotonoMacBook-puro.local>
2019-06-11 18:46:56 +0900

    add home

--- Auction/Auction/ContentView.swift
+++ Auction/Auction/ContentView.swift
@@ -19,12 +19,31 @@ struct ContentView : View {
 }
 
 struct HomeView : View {
+    var index: Int {
+        return Int.random(in: 0..<items.count)
+    }
+    
     var body: some View {
         NavigationView {
             List {
-                Text("Home")
+                ForEach(items) { item in
+                    HomeRow(items: [items[self.index], items[self.index], items[self.index]])
+                }
             }
             .navigationBarTitle(Text("Home"))
+        }
+    }
+}
+
+struct HomeRow : View {
+    var items: [Item]
+    let width: CGFloat = (UIScreen.main.bounds.width - 48) / 3
+    
+    var body: some View {
+        HStack {
+            ForEach(items) { item in
+                Image(item.image).resizable().frame(width: self.width, height: self.width)
+            }
         }
     }
 }
